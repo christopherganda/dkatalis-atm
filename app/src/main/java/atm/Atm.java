17211c1280c6d03b20b12dc59716c5cb96d2721b
christopherganda <christopher.ganda@gmail.com>
2025-10-31 07:19:27 +0700

    add deposit logic

--- app/src/main/java/atm/Atm.java
+++ app/src/main/java/atm/Atm.java
@@ -1,4 +1,3 @@
-
 package atm;
 
 import java.math.BigDecimal;
@@ -6,8 +5,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Atm {
-  private Map<String, BigDecimal> users = new HashMap<String, BigDecimal>();
-  private String loggedInUser = null;
+  private Map<String, User> users = new HashMap<String, User>();
+  private User loggedInUser = null;
 
   // improvement: how to handle concurrent requests
   public void login(String username) {
@@ -19,8 +18,21 @@ public class Atm {
       throw new IllegalStateException("Currently logged in as: " + loggedInUser);
     }
 
-    users.putIfAbsent(username, BigDecimal.valueOf(0));
-    loggedInUser = username;
-    System.out.println("Successfully logged in as: " + loggedInUser);
+    users.putIfAbsent(username, new User(username));
+    loggedInUser = users.get(username);
+    System.out.println("Successfully logged in as: " + loggedInUser.getUsername());
+  }
+
+  public void deposit(BigDecimal amount) {
+    if (loggedInUser == null) {
+      throw new IllegalStateException("You must logged in before doing deposit");
+    }
+
+    if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+      throw new IllegalArgumentException("Amount should not be lower than 0");
+    }
+
+    loggedInUser.deposit(amount);
+    System.out.println("Current balance after deposit: " + loggedInUser.getBalance());
   }
 }
