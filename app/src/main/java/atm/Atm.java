fea85cdd6a91243d7e27eb64f4559063b24eb1eb
christopherganda <christopher.ganda@gmail.com>
2025-11-01 12:48:13 +0700

    refactor and add notes

--- app/src/main/java/atm/Atm.java
+++ app/src/main/java/atm/Atm.java
@@ -11,16 +11,17 @@ public class Atm {
   private Map<String, User> users = new HashMap<String, User>();
   private User loggedInUser = null;
 
+  private static final String ERR_NO_USER_LOGGED_IN = "You must be logged in to perform this action.";
+  private static final String ERR_INVALID_AMOUNT = "Amount should be greater than or equal to 0.";
+  private static final String ERR_INVALID_USERNAME = "Username cannot be empty";
+  private static final String ERR_SELF_TRANSFER = "Cannot transfer to self.";
+  private static final String ERR_USER_NOT_FOUND = "Target user does not exist.";
+
   // improvement: how to handle concurrent requests
   public void login(String username) {
-    if (username == null || username.isBlank()) {
-      throw new IllegalArgumentException("Username cannot be empty");
-    }
-
-    if (loggedInUser != null) {
-      throw new IllegalStateException("Currently logged in as: " + loggedInUser);
-    }
-
+    validateUsername(username);
+    validateMultipleLoginAttempt();
+    
     users.putIfAbsent(username, new User(username));
     loggedInUser = users.get(username);
     System.out.printf("Hello, %s!\n", loggedInUser.getUsername());
@@ -28,13 +29,8 @@ public class Atm {
   }
 
   public void deposit(BigDecimal amount) {
-    if (loggedInUser == null) {
-      throw new IllegalStateException("You must logged in before doing deposit");
-    }
-
-    if (amount.compareTo(BigDecimal.ZERO) < 0) {
-      throw new IllegalArgumentException("Amount should not be lower than 0");
-    }
+    validateLoggedIn();
+    validateAmount(amount);
 
     amount = processDepositOwes(amount);
     loggedInUser.addBalance(amount);
@@ -42,13 +38,8 @@ public class Atm {
   }
 
   public void withdraw(BigDecimal amount) {
-    if (loggedInUser == null) {
-      throw new IllegalStateException("You must logged in before doing deposit");
-    }
-
-    if (amount.compareTo(BigDecimal.ZERO) < 0) {
-      throw new IllegalArgumentException("Amount should not be lower than 0");
-    }
+    validateLoggedIn();
+    validateAmount(amount);
 
     if (loggedInUser.getBalance().compareTo(amount) < 0) {
       throw new IllegalStateException("Your remaining balance is " + loggedInUser.getBalance());
@@ -62,71 +53,25 @@ public class Atm {
   // 1. if loggedInUser is owed by the targetUser, if yes we need to adjust the owes and owed, if amount has remaining, we deduct loggedInUser's balance
   // 2. if loggedInUser is not owed by targetUser, we deduct loggedInUser's balance, if amount is greater than balance, then we need to add owes and owed to
   public void transfer(String receiverUsername, BigDecimal amount) {
-    if (loggedInUser == null) {
-      throw new IllegalStateException("You must logged in before doing transfer");
-    }
+    validateLoggedIn();
+    validateAmount(amount);
+    validateSelfTransfer(receiverUsername);
 
-    if (amount.compareTo(BigDecimal.ZERO) < 0) {
-      throw new IllegalArgumentException("Amount should not be lower than 0");
-    }
-
-    if (receiverUsername.equals(loggedInUser.getUsername())) {
-      throw new IllegalStateException("Cannot transfer to self");
-    }
-
-    User receiver = users.get(receiverUsername);
-    if (receiver == null) {
-      throw new IllegalStateException("Target user does not exist.");
-    }
-
+    User receiver = getUser(receiverUsername);
     BigDecimal remainingAmount = processTransferOwedBy(receiver, amount);
-    if (remainingAmount.compareTo(BigDecimal.ZERO) > 0) {
-      BigDecimal senderBalance = loggedInUser.getBalance();
-      if (senderBalance.compareTo(remainingAmount) >= 0) {
-        loggedInUser.subtractBalance(remainingAmount);
-        receiver.addBalance(remainingAmount);
-        printTransferredToMsg(receiverUsername, remainingAmount);
-      } else {
-        BigDecimal overBalance = remainingAmount.subtract(senderBalance);
-        loggedInUser.subtractBalance(senderBalance); // set balance to 0
-        receiver.addBalance(senderBalance);
-        receiver.addOwedBy(loggedInUser.getUsername(), overBalance);
-        loggedInUser.addOwesTo(receiverUsername, overBalance);
-        printTransferredToMsg(receiverUsername, senderBalance);
-      }
-    }
+    processBalanceTransfer(receiver, remainingAmount);
     printYourBalance();
   }
 
   public void logout() {
     if (loggedInUser == null) {
-      throw new IllegalStateException("System is not logged in to any user");
+      throw new IllegalStateException(ERR_NO_USER_LOGGED_IN);
     }
     System.out.printf("Goodbye, %s!\n", loggedInUser.getUsername());
     loggedInUser = null;
   }
 
-  // The logic idea behind owesTo and owedBy
-  // user1 balance 100
-  // user2 0
-
-  // user1 transfer 150 to user 2
-  // user1 balance 0, user2 balance 150
-  // user1 owesTo(Queue) user2 50
-  // user2 owedBy(Map) user1 50
-
-  // let's say user1 do deposit
-  // user1 deposit 50
-  // user1 balance 0, user2 balance 150
-  // user1 owesTo(Queue) user2 has to be removed
-  // user2 owedBy(Map) user1 has to be removed
-
-  // let's say if user2 do the transfer
-  // user2 transfer to user1 50
-  // user1 balance 50, user2 balance 100
-  // user2 owedBy(map) user1 has to be removed
-  // user1 owesTo(Queue) has to be removed
-
+  // Private Helpers
   // Iterate loggedInUser.getOwesTo
   // then deduct amount based on queue
   // or remove from queue if amount is greater than owes amount.
@@ -135,6 +80,8 @@ public class Atm {
     while(!owedTo.isEmpty() && amount.compareTo(BigDecimal.ZERO) > 0) {
       Debt debt = owedTo.peek();
       User userOwedTo = users.get(debt.getUsername());
+      // If amount is greater than debt amount, we settle all the debt
+      // else we deduct the debt according to amount
       if (amount.compareTo(debt.getAmount()) > 0) {
         amount = amount.subtract(debt.getAmount());
         userOwedTo.deductOrRemoveOwedBy(loggedInUser.getUsername(), debt.getAmount());
@@ -160,11 +107,15 @@ public class Atm {
       BigDecimal debtAmount = owedBy.get(receiverUsername);
       Queue<Debt> owesTo = receiver.getOwesTo();
 
-      if (amount.compareTo(debtAmount) >= 0) { // amount transferred >= debtAmount
+      // If transfer amount is greater than the owedBy debt, we will
+      // remove owedBy and from queue, remaining amount will deduct balance
+      // If transfer amount is lower than owedBy debt,
+      // we will deduct debtAmount from owedBy and queue.
+      if (amount.compareTo(debtAmount) >= 0) {
         amount = amount.subtract(debtAmount);
         owedBy.remove(receiverUsername);
         removeDebtFromQueue(owesTo, loggedInUser.getUsername());
-      } else { // amount < debtAmount
+      } else {
         owedBy.put(receiverUsername, debtAmount.subtract(amount));
         for (Debt debt : owesTo) {
           if (debt.getUsername().equals(loggedInUser.getUsername())) {
@@ -177,6 +128,29 @@ public class Atm {
     }
 
     return amount;
+  }
+
+  // If senderBalance is greater than remaining amount to be transferred,
+  // Then we will just deduct senderBalance without any owe
+  // Else we will deduct based on senderBalance
+  // And we will process owedBy and OwesTo
+  private void processBalanceTransfer(User receiver, BigDecimal remainingAmount) {
+    String receiverUsername = receiver.getUsername();
+    if (remainingAmount.compareTo(BigDecimal.ZERO) > 0) {
+      BigDecimal senderBalance = loggedInUser.getBalance();
+      if (senderBalance.compareTo(remainingAmount) >= 0) {
+        loggedInUser.subtractBalance(remainingAmount);
+        receiver.addBalance(remainingAmount);
+        printTransferredToMsg(receiverUsername, remainingAmount);
+      } else {
+        BigDecimal overBalance = remainingAmount.subtract(senderBalance);
+        loggedInUser.subtractBalance(senderBalance);
+        receiver.addBalance(senderBalance);
+        receiver.addOwedBy(loggedInUser.getUsername(), overBalance);
+        loggedInUser.addOwesTo(receiverUsername, overBalance);
+        printTransferredToMsg(receiverUsername, senderBalance);
+      }
+    }
   }
 
   private void removeDebtFromQueue(Queue<Debt> owesTo, String username) {
@@ -203,5 +177,43 @@ public class Atm {
 
   private void printTransferredToMsg(String username, BigDecimal amount) {
     System.out.printf("Transferred %s to %s\n", Formatter.formatCurrencyWithoutComma(amount), username);
+  }
+
+  private void validateUsername(String username) {
+    if (username == null || username.isBlank()) {
+      throw new IllegalArgumentException(ERR_INVALID_USERNAME);
+    }
+  }
+
+  private void validateMultipleLoginAttempt() {
+    if (loggedInUser != null) {
+      throw new IllegalStateException("Currently logged in as: " + loggedInUser);
+    }
+  }
+
+  private void validateLoggedIn() {
+    if (loggedInUser == null) {
+      throw new IllegalStateException(ERR_NO_USER_LOGGED_IN);
+    }
+  }
+
+  private void validateAmount(BigDecimal amount) {
+    if (amount.compareTo(BigDecimal.ZERO) < 0) {
+      throw new IllegalArgumentException(ERR_INVALID_AMOUNT);
+    }
+  }
+  
+  private void validateSelfTransfer(String receiverUsername) {
+    if (receiverUsername.equals(loggedInUser.getUsername())) {
+      throw new IllegalStateException(ERR_SELF_TRANSFER);
+    }
+  }
+
+  private User getUser(String username) {
+    User user = users.get(username);
+    if (user == null) {
+      throw new IllegalStateException(ERR_USER_NOT_FOUND);
+    }
+    return user;
   }
 }
