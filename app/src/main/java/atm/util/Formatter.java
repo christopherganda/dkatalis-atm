f275b3d4769e8e2d12ab3053a4b116a8437fc2d3
christopherganda <christopher.ganda@gmail.com>
2025-11-02 14:45:06 +0700

    remove unused method

--- app/src/main/java/atm/util/Formatter.java
+++ app/src/main/java/atm/util/Formatter.java
@@ -1,16 +1,9 @@
 package atm.util;
 
 import java.math.BigDecimal;
-import java.text.NumberFormat;
-import java.util.Locale;
 
 public final class Formatter {
   private Formatter() {}
-
-  public static String formatCurrency(BigDecimal amount) {
-    NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(Locale.US);
-    return currencyFormatter.format(amount);
-  }
 
   public static String formatCurrencyWithoutComma(BigDecimal amount) {
     return String.format("$%.0f", amount);
